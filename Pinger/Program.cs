55310c8c5258c69353c505787f3197327a919cdb
Dave Potts <dave@goopot.co.uk>
2021-10-25 17:53:04 +0000

    Tidy code style

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -7,6 +7,6 @@ using Pinger;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+await builder.Build().RunAsync().ConfigureAwait(false);
